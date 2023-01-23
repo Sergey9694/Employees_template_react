f175f67c7c3aba175c7152a67f7345b329dd57f6
Sergey9694 <drobilenko94@mail.ru>
2023-01-23 15:46:19 +0300

    added dynamic substitution of classes and list of employees

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -6,6 +6,13 @@ import EmployeesAddForm from '../employees-add-form/employees-add-form';
 import './app.css';
 
 function App() {
+    // Как будто данные с сервера
+    const data = [
+        {name: 'John C.' , salary: 800, increase: false},
+        {name: 'Alex M.' , salary: 3000, increase: true},
+        {name: 'Carl W.' , salary: 5000, increase: false},
+    ];
+
     return (
         <div className="app">  
             <AppInfo/>
@@ -14,8 +21,8 @@ function App() {
                 <SearchPanel/>
                 <AppFilter/>
             </div>
-
-            <EmployeesList/>
+            
+            <EmployeesList data={data}/> 
             <EmployeesAddForm/>
         </div>
     );
--- src/components/employees-list/employees-list.js
+++ src/components/employees-list/employees-list.js
@@ -1,12 +1,17 @@
 import EmployeesListItem from '../employees-list-item/employees-list-item';
 import './employees-list.css';
 
-const EmployeesList = () => {
+const EmployeesList = ({data}) => { //в компонент приходит data
+
+    const elements = data.map(item => { // перебираем эл-ты массива
+        return (
+            <EmployeesListItem {...item}/> //разложили св-ва через spread
+        )
+    })
+
     return (
         <ul className="app-list app-group">
-            <EmployeesListItem/>
-            <EmployeesListItem/>
-            <EmployeesListItem/>
+            {elements}
         </ul>
     )
 }
