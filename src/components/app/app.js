98373ab5ad2871a2815c13d12c49b7d91a809678
Sergey9694 <drobilenko94@mail.ru>
2023-02-01 14:12:07 +0300

    added the ability to add employees

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,31 +1,68 @@
+import { Component } from 'react';
+
 import AppInfo from '../app-info/app-info';
 import SearchPanel from '../search-panel/search-panel';
 import AppFilter from '../app-filter/app-filter';
 import EmployeesList from '../employees-list/employees-list';
 import EmployeesAddForm from '../employees-add-form/employees-add-form';
+
 import './app.css';
 
-function App() {
-    // Как будто данные с сервера
-    const data = [
-        {name: 'John C.' , salary: 800, increase: false, id: 1},
-        {name: 'Alex M.' , salary: 3000, increase: true, id: 2},
-        {name: 'Carl W.' , salary: 5000, increase: false, id: 3},
-    ];
+class App extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            data: [     // Как будто данные с сервера
+                {name: 'John C.' , salary: 800, increase: false, id: 1},
+                {name: 'Alex M.' , salary: 3000, increase: true, id: 2},
+                {name: 'Carl W.' , salary: 5000, increase: false, id: 3},
+            ]
+        }
+        this.maxId = 4;
+    }
 
-    return (
-        <div className="app">  
-            <AppInfo/>
+    deleteItem = (id) => {
+        this.setState(({data}) => {
+            return {
+                data: data.filter(item => item.id !== id)
+            }
+        })
+    }
 
-            <div className="search-panel">
-                <SearchPanel/>
-                <AppFilter/>
+    addItem = (name, salary) => {
+        const newItem = {
+            name,
+            salary,
+            increase: false,
+            id: this.maxId++
+        }
+        this.setState(({data}) => {
+            const newArr = [...data, newItem]
+            return {
+                data: newArr
+            }
+        })
+
+    }
+
+    render() {
+        return (
+            <div className="app">  
+                <AppInfo/>
+
+                <div className="search-panel">
+                    <SearchPanel/>
+                    <AppFilter/>
+                </div>
+                
+                <EmployeesList 
+                    data={this.state.data}
+                    onDelete={this.deleteItem}/> 
+                <EmployeesAddForm
+                    onAddEmployees={this.addItem}/>
             </div>
-            
-            <EmployeesList data={data}/> 
-            <EmployeesAddForm/>
-        </div>
-    );
+        );
+    }
 }
 
 export default App;
